b2162cbf7c9522620c03ba9f3c583fe844354593
nathanael early <nathanael.early@gmail.com>
2025-09-15 10:20:38 -0400

    Organizaton update
    i added folders because when redirecting scenes you need to do it by file locations so the sooner we can get it organized the less it breaks later

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -9,7 +9,7 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://options_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 	pass # 
 
 
--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -5,5 +5,5 @@ extends Control
 
 
 func _on_backbutton_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	pass # Replace with function body.
