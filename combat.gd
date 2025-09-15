a2beba7bc49573f14df5cd18e8cabcfde953002f
Stephen Cox <stevecox2005@gmail.com>
2025-09-15 16:44:01 -0400

    Punch animation implemented
    Added the animation and keybinding for punches.

--- combat.gd
+++ combat.gd
@@ -1,17 +1,22 @@
 extends Node2D
 @onready var animation: AnimationPlayer = $Animation
-@export var isdodge = true # is your ability to get hit(if not dodgeing you can get smacked)
+@export var isdodge = false # is your ability to get hit(if not dodgeing you can get smacked)
+@export var ispunch = false # is your boolean for punching.
 @export var canmove = true # is your inability to move(if you cant move you cant dodge)
 
-
 func _process(delta):
-	if canmove == true:# this is if your able to move so attacks and the like go here
-		if Input.is_action_pressed("dodgeright"):
-			animation.play("dodgeright")
-			
-			# start timer for protection OR instead make it do a thing at the end of the animation
-			pass
-		if Input.is_action_pressed("dodgeleft"):
-			animation.play("dodgeleft")
-			# start timer for protection OR instead make it do a thing at the end of the animation
-			pass
+	if Input.is_action_pressed("dodgeright"):
+		if canmove == true: # this is if you're able to move so attacks and the like go here
+			if isdodge == false: #this is if you're dodging or not.
+				animation.play("dodgeright")
+				pass
+	if Input.is_action_pressed("dodgeleft"):
+		if canmove == true: # this is if you're able to move so attacks and the like go here
+			if isdodge == false: #this is if you're dodging or not.
+				animation.play("dodgeleft")
+				pass
+	if Input.is_action_pressed("punch"):
+		if canmove == true: # this is if you're able to move so attacks and the like go here
+			if isdodge == false: #this is if you're dodging or not.
+				animation.play("punch")
+				pass
