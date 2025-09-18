cfb6a1120bbe9038189adc6865596f755585f016
Stephen Cox <stevecox2005@gmail.com>
2025-09-18 16:43:30 -0400

    Fixed hooks/added left & right hits
    Hook animation is smoother and works properly. Added left hits and right hits with the Q and E buttons!

--- Scripts/combat.gd
+++ Scripts/combat.gd
@@ -3,10 +3,20 @@ extends Node2D
 @onready var character = $Character
 @export var isdodge = false # is your ability to get hit(if not dodgeing you can get smacked)
 @export var ispunch = false # is your boolean for punching.
-@export var isHoldingHook = false
+@export var isHolding = false
 @export var canmove = true # is your inability to move(if you cant move you cant dodge)
 
-func _process(delta):
+var originalPos = Vector2(588.0, 401.0)
+const HOLD_TIME_THRESHOLD = 0.5
+const SHAKE_STRENGTH = 10.0
+var heldTime = 0
+
+var pressTimes = {
+	"leftAttack": -1.0,
+	"rightAttack": -1.0
+}
+
+func _input(event):
 	if Input.is_action_just_pressed("dodgeright"):
 		if canmove == true: # this is if you're able to move so attacks and the like go here
 			if isdodge == false: #this is if you're dodging or not.
@@ -17,33 +27,46 @@ func _process(delta):
 			if isdodge == false: #this is if you're dodging or not.
 				animation.play("dodgeleft")
 				pass
-	if Input.is_action_pressed("hook"):
-		if canmove == true:
-			if isdodge == false:
-				if isHoldingHook == false:
-					isHoldingHook = true
-					start_shake()
-	else:
-		isHoldingHook = false
-	if Input.is_action_just_pressed("punch"):
-		if canmove == true: # this is if you're able to move so attacks and the like go here
-			if isdodge == false: #this is if you're dodging or not.
-				animation.play("punch")
-				pass
-
-func start_shake():
-	if not isHoldingHook:
-		animation.play("punch")
-		return
-	var originalPos = character.position
-	var shakeStrength = 10.0
-	var shakeDuration = 0.2
+		
+	if Input.is_action_pressed("attackLeft"):
+		if isHolding == false:
+			pressTimes["leftAttack"] = Time.get_ticks_msec() / 1000.0
+			isHolding = true
+		heldTime = (Time.get_ticks_msec()/1000.0) - pressTimes["leftAttack"]
+		if heldTime >= HOLD_TIME_THRESHOLD and isHolding == true:
+			start_shake()
+			heldTime = 0
+	if Input.is_action_just_released("attackLeft"):
+		heldTime = (Time.get_ticks_msec()/1000.0) - pressTimes["leftAttack"]
+		if heldTime < HOLD_TIME_THRESHOLD and isHolding == true:
+			animation.play("punch")
+		heldTime = 0
+		isHolding = false
+		if character.position != originalPos:
+			character.position = originalPos
+			animation.play("punch")
+		
+	if Input.is_action_pressed("attackRight"):
+		if isHolding == false:
+			pressTimes["rightAttack"] = Time.get_ticks_msec() / 1000.0
+			isHolding = true
+		heldTime = (Time.get_ticks_msec()/1000.0) - pressTimes["rightAttack"]
+		if heldTime >= HOLD_TIME_THRESHOLD and isHolding == true:
+			start_shake()
+			heldTime = 0
+	if Input.is_action_just_released("attackRight"):
+		heldTime = (Time.get_ticks_msec()/1000.0) - pressTimes["rightAttack"]
+		if heldTime < HOLD_TIME_THRESHOLD and isHolding == true:
+			animation.play("punch")
+		heldTime = 0
+		isHolding = false
+		if character.position != originalPos:
+			character.position = originalPos
+			animation.play("punch")
 	
-	var tween = character.create_tween()
-	tween.tween_property(character, "position", originalPos + Vector2(-shakeStrength, 0), shakeDuration / 2)
-	tween.tween_property(character, "position", originalPos + Vector2(shakeStrength, 0), shakeDuration / 2)
-	tween.tween_property(character, "position", originalPos + Vector2(0, -shakeStrength), shakeDuration / 2)
-	tween.tween_property(character, "position", originalPos + Vector2(0, shakeStrength), shakeDuration / 2)
-	tween.tween_property(character, "position", originalPos, shakeDuration / 2)
-	await tween.finished
-	start_shake()
+func start_shake():
+	character.position = originalPos + Vector2(randf_range(-SHAKE_STRENGTH, SHAKE_STRENGTH), randf_range(-SHAKE_STRENGTH, SHAKE_STRENGTH))
+	await get_tree().process_frame
+
+	if isHolding:
+		start_shake()
