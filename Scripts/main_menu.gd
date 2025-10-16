b61a1d641a4c7a163eefc0d8075b858ac351ef74
JAMSalmon1 <johnamonroe6@gmail.com>
2025-10-16 15:43:56 -0400

    Damage to player and next button

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,15 +4,30 @@ extends Control
 @onready var start_button: Button = $VBoxContainer/StartButton
 
 
-func _on_start_button_pressed() -> void:	
+func _on_start_button_pressed() -> void:	#Connects the start menu .
 	get_tree().change_scene_to_file("res://Scenes/combat.tscn")
-
-
-func _on_options_button_pressed() -> void:
+#Both buttons are within the start menu
+func _on_options_button_pressed() -> void: 
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 	pass # 
 
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+	pass #
+
+
+#func _on_next_button_pressed() -> void #Connects to the intro once next is clicked.
+	get_tree().change_sence_to_file("res://Scenes/intro.tscn")
+	pass #
+
+
+
+	
+	#func _on_next_button_pressed() -> void #connects to the locker menu when clicked.
+	get_tree().change_sence_to_file("res://Scenes/locker.tscn")
+	pass #
+	
+	#func _on_next_button_pressed() -> void #connects to the combat menu
+	get_tree().change_sence_to_file("res://Scenes/combat.tscn")
 	pass #
