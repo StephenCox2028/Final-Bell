ff24adeee7bf817d58d6d649877513c038c2284c
Mirza-Selimovic06 <mselimovic.8@gmail.com>
2025-12-03 19:16:13 -0500

    Edits to animation
    Puppeteer animations added need help figuring out why my script isn't working might be how the nodes are set up.

--- Scripts/Bosses scripts/Puppet1.gd
+++ Scripts/Bosses scripts/Puppet1.gd
@@ -1,13 +1,56 @@
+var max_health = Global.bosses.health
+var phase_2_initialized = false  # Prevent repeated phase 2 setup
+
 func Actions(boss_node):
-	print(boss_node.health, boss_node.stamina, boss_node.power)
-	var action = randi_range(1, 3)
-	match action:# these play anmations but we dont gottem yet
-		1:
-			boss_node.animation_player.play("idle")
-			pass
-		2:
-			boss_node.animation_player.play("block")
-			pass
-		3:
-			boss_node.animation_player.play("block")
-			pass
+	print(Global.bosses.health, Global.bosses.stamina, Global.bosses.power)
+	
+	var action
+	var half_hp = max_health / 2
+	
+	# Check for phase transition
+	if Global.bosses.phase == 1 and Global.bosses.health <= half_hp:
+		transition_to_phase_2(boss_node)
+		return  # Exit early during transition
+	
+	# Phase 1 actions
+	if Global.bosses.phase == 1:
+		action = randi_range(1, 6)
+		match action:
+			1:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer1Idle")
+			2:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer1left_punch")
+			3:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer1Block")
+			4:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer1right_punch")
+			5:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer1left_hook")
+			6:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer1right_hook")
+	
+	# Phase 2 actions
+	elif Global.bosses.phase == 2:
+		action = randi_range(1, 4)
+		match action:
+			1:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer2Idle")
+			2:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer2Jab")
+			3:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer2Block")
+			4:
+				boss_node.get_node("AnimatedSprite2D").play("Puppeteer2Cross")
+
+func transition_to_phase_2(boss_node):
+	if phase_2_initialized:
+		return  # Already transitioned
+	
+	phase_2_initialized = true
+	Global.bosses.phase = 2
+	Global.bosses.health = 50  # Heal to 50
+	Global.bosses.power *= 1.5  # Increase power once
+	boss_node.get_node("Timer").wait_time = 0.5
+	
+	# Play transition animations in sequence
+	boss_node.get_node("AnimatedSprite2D").play("PuppeteerThrowP1")
--- Scripts/bosses.gd
+++ Scripts/bosses.gd
@@ -2,9 +2,6 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var timer: Timer = $Timer
-@export var health = 1
-@export var stamina = 1
-@export var power = 1
 @onready var warningTimer: Timer = $WarningTimer
 @onready var skeletonMusic: AudioStreamPlayer = $SkeletonMusic
 @onready var devilMusic: AudioStreamPlayer = $DevilMusic
@@ -14,30 +11,31 @@ extends Node2D
 #var skeletin = load("res://Scripts/Skeleton.gd").new()# so i can add more later easily
 #var slime = load("res://Scripts/Slime.gd").new()# so i can add more later easily
 var spider = load("res://Scripts/Bosses scripts/Spider.gd").new()
-var puppeteer = load("res://Scripts/Bosses scripts/Puppet1.gd").new()
-var racoon = load("res://Scripts/Bosses scripts/Racoon.gd").new()
+var puppeteer1 = load("res://Scripts/Bosses scripts/Puppet1.gd").new()
+#var puppeteer2 = load("res://Scripts.Bosses scripts/Puppet2.gd").new()
+
 var action : int
 var safe = false # used to detect if blocking or dodging
 var enmActions = []
 
 func _ready():
-		enmActions = [puppeteer]#add more latter
+		enmActions = [puppeteer1] 
 
 		match Global.Boss_counter:
 				1:
 						action = 0
-						change_stats(20,20,20,5.0, 5.0, 0)
-						animated_sprite_2d.play("puppeteerIdle")
+						change_stats(50,20,20,5.0, 5.0, 0)
+						animated_sprite_2d.play("Puppeteer1Idle")
 						pass#change sprites for scertain enemys(reason why their split is also because of the amount of>
 				2:
 						action = 0
-						change_stats(20,20,20,5.0, 5.0, 0)
-						animated_sprite_2d.play("puppeteerIdle")
+						change_stats(50,20,20,5.0, 5.0, 0)
+						animated_sprite_2d.play("Puppeteer1Idle")
 						pass
 				3:
 						action = 0
-						change_stats(20,20,20,5.0, 5.0, 0)
-						animated_sprite_2d.play("puppeteerIdle")
+						change_stats(50,20,20,5.0, 5.0, 0)
+						animated_sprite_2d.play("Puppeteer1Idle")
 						pass
 
 
@@ -45,11 +43,12 @@ func _on_timer_timeout() -> void:
 		enmActions[action].call("Actions", self)
 
 func change_stats(hp,sta,pow,max,min, blockchance):
-	Global.bosses.health 		= hp
-	Global.bosses.stamina 		= sta
-	Global.bosses.power 		= pow
+	Global.bosses.health = hp
+	Global.bosses.stamina = sta
+	Global.bosses.power = pow
 	Global.bosses.difficultyMax = max
 	Global.bosses.difficultyMin = min
 	Global.enemy_max = hp
-	Global.bosses.block_chance= blockchance
+	Global.bosses.block_chance = blockchance
+	Global.bosses.phase = 1
 	pass
